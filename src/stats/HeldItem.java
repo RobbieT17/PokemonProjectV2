ee0cbeecaed1bb893b1ffc8d6b58182606fc3801
RobbieT17 <robbiewthurston@gmail.com>
2024-09-29 00:34:32 -0400

    added more items

--- src/stats/HeldItem.java
+++ src/stats/HeldItem.java
@@ -9,6 +9,13 @@ public class HeldItem extends Effect {
 
 // Public Class Variables
     public static final String ASSAULT_VEST_ID = "Assault-Vest";
+    public static final String CHOICE_BAND_ID = "Choice-Band";
+    public static final String CHOICE_SCARF_ID = "Choice-Band";
+    public static final String CHOICE_SPECS_ID = "Choice-Band";
+    public static final String LEFTOVERS_ID = "Leftovers";
+    public static final String MUSCLE_BAND_ID = "Muscle-Band";
+    public static final String WISE_GLASSES_ID = "Wise-Glasses";
+    public static final String BLACK_SLUDGE_ID = "Black-Sludge";
 
 
 // Object
@@ -32,6 +39,82 @@ public class HeldItem extends Effect {
         p.events().addEventListener(flags[1], name, e -> {
             if (!EventData.isTarget(p, e)) return;
             p.specialDefense().setMod(150);
+        });
+
+        return new HeldItem(p, name, flags);
+	}
+
+    // Restore 1/16 of Max HP at the end of each round
+    public static HeldItem leftovers(Pokemon p) {
+        String name = HeldItem.LEFTOVERS_ID;
+        String[] flags = new String[] {GameEvent.END_OF_ROUND};
+
+        p.events().addEventListener(flags[0], name, e -> p.restoreHpPercentMaxHP(1.0 / 16.0, " from its Leftovers"));
+
+        return new HeldItem(p, name, flags);
+	}
+
+    // Boosts physical-move power by 10%
+    public static HeldItem muscleBand(Pokemon p) {
+        String name = HeldItem.MUSCLE_BAND_ID;
+        String[] flags = new String[] {GameEvent.DAMAGE_MULTIPLIER};
+
+        p.events().addEventListener(flags[0], name, e -> {
+            if (!(EventData.isUser(p, e) && e.moveUsed.isCategory(Move.PHYSICAL))) return;
+            e.otherMoveMods *= 1.1;
+        });
+
+        return new HeldItem(p, name, flags);
+	}
+
+    // Boosts special-move power by 10%
+    public static HeldItem wiseGlasses(Pokemon p) {
+        String name = HeldItem.WISE_GLASSES_ID;
+        String[] flags = new String[] {GameEvent.DAMAGE_MULTIPLIER};
+
+        p.events().addEventListener(flags[0], name, e -> {
+            if (!(EventData.isUser(p, e) && e.moveUsed.isCategory(Move.SPECIAL))) return;
+            e.otherMoveMods *= 1.1;
+        });
+
+        return new HeldItem(p, name, flags);
+	}
+
+    // Poison Type restore 1/16 of their Max HP, all other types lose that amount
+    public static HeldItem blackSludge(Pokemon p) {
+        String name = HeldItem.BLACK_SLUDGE_ID;
+        String[] flags = new String[] {GameEvent.END_OF_ROUND};
+
+        p.events().addEventListener(flags[0], name, e -> {
+            double percent = 1.0 / 16.0;
+            if (p.isType(Type.POISON)) {
+                p.restoreHpPercentMaxHP(percent, name);
+                return;
+            }
+            p.takeDamagePercentMaxHP(percent, name);
+        });
+
+        return new HeldItem(p, name, flags);
+	}
+
+    // Boosts Attack stat by 50% but forces Pokemon to use first move selected (rests after switch out)
+    public static HeldItem choiceBand(Pokemon p) {
+        String name = HeldItem.CHOICE_BAND_ID;
+        String[] flags = new String[] {GameEvent.MOVE_SELECTION, GameEvent.SWITCH_OUT, GameEvent.DAMAGE_MULTIPLIER};
+
+        Move m = p.moveSelected(); // NULL when sent out
+
+        p.events().addEventListener(flags[0], name, e -> {
+            if (m == null) return;
+            p.setMove(m);
+        });
+
+        p.events().addEventListener(flags[1], name, e -> {
+            
+        });
+
+        p.events().addEventListener(flags[2], name, e -> {
+            
         });
 
         return new HeldItem(p, name, flags);
